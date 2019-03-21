c9609ad918cb9c274e5204a10baaf0bfff4da4bf
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-03-21 16:53:53 -0500

    Styling update and JS variables defined

---
+++ index.js
@@ -0,0 +1,6 @@
+const expenseList = document.querySelector('expense-list');
+const expenseName = document.querySelector('item');
+const expenseAmt = document.querySelector('amount');
+const addItemBtn = document.querySelector('add-bill');
+
+
